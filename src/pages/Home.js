8b884246be78ef21f139711c185fbecbea2ba96a
dhanesh <dhaneshpk359@gmail.com>
2023-12-22 16:59:09 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "../fetures/Navbar/Nav";
+import ProductList from "../fetures/product/ProductList";
+
+function Home() {
+  return (
+    <div>
+      <Nav>
+        <ProductList> </ProductList>
+      </Nav>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignUp from '../auth/components/Signup'
+
+function SignUpPage() {
+  return (
+    <SignUp/>
+  )
+}
+
+export default SignUpPage
